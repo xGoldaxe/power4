1a9fe203db991e0915715707bac179133ebf802d
Goldaxe <pierreleveque.pro@gmail.com>
2021-02-04 07:37:32 +0100

    ea

--- controllers/user.js
+++ controllers/user.js
@@ -45,9 +45,9 @@ exports.login = (req, res, next) => {
                         )
                     });
                 })
-                .catch(error => res.status(500).json({ error }));
+                .catch(error => res.status(401).json({ error }));
         })
-        .catch(error => res.status(500).json({ error }));
+        .catch(error => res.status(404).json({ error }));
 };
 
 exports.information = (req, res, next) => {
